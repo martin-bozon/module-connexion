10d199633e2236da2120dba2543f8582d3752f70
Martin-Bozon <62134476+Martin-Bozon@users.noreply.github.com>
2020-05-19 16:48:15 +0200

    mise en page admin, ok

--- admin.php
+++ admin.php
@@ -12,37 +12,54 @@
     <link rel="stylesheet" href="style_mc.css"/>
     <title>Admin</title>
 </head>
+
 <body>
-    <table id="tableau_users">
-        <thead>
-            <?php
-                foreach($info_users[0] as $champ => $info)
-                    {
-            ?>
-            <th><?php echo $champ;?></th>
+    <header>
+        <section id="Hgauche">
+            <h2><a href="index.php">Accueil</a></h2>
+        </section>
+        <section id="Hdroite">
+            <h2><a href="inscription.php">Inscription</a></h2>
+        </section>
+    </header>
+    <main>
+        <table id="tableau_users">
+            <thead>
                 <?php
-                    }
-                ?>
-        </thead>
-        <tbody>
-            <?php
-                foreach($info_users as $user)
-                    {
-            ?>
-            <tr>
-                <?php
-                    foreach($user as $champ => $info)
+                    foreach($info_users[0] as $champ => $info)
                         {
                 ?>
-                <td><?php echo $info;?></td>
+                <th><?php echo $champ;?></th>
                     <?php
                         }
                     ?>
-            </tr>
+            </thead>
+            <tbody>
                 <?php
-                    }
+                    foreach($info_users as $user)
+                        {
                 ?>
-        </tbody>
-    </table>
+                <tr>
+                    <?php
+                        foreach($user as $champ => $info)
+                            {
+                    ?>
+                    <td><?php echo $info;?></td>
+                        <?php
+                            }
+                        ?>
+                </tr>
+                    <?php
+                        }
+                    ?>
+            </tbody>
+        </table>
+    </main>
+
+    <footer>
+        <h2><a href="index.php">Accueil</a></h2>
+        <h3>Copyright Martin Bozon</h3>
+        <h2><a href="https://github.com/martin-bozon">GitHub</a></h2>
+    </footer>
 </body>
 </html>
