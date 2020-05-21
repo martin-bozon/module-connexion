33add0c4e94ceff264f85c42b2c4b978a70ddc68
Martin-Bozon <62134476+Martin-Bozon@users.noreply.github.com>
2020-05-21 15:21:48 +0200

    apparement ca marche

--- profil.php
+++ profil.php
@@ -5,7 +5,7 @@
         session_destroy();
         header('Location:index.php');
     }
-?>
+    ?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -14,57 +14,61 @@
     <title>Profil</title>
 </head>
     <body>
-            <?php
-                
+            <?php                    
                 if(isset($_SESSION["login"]))
                     {
+                       
                         $connexionbd = mysqli_connect("localhost" , "root" , "" , "moduleconnexion");
                         $requeteinfo = "SELECT * FROM utilisateurs WHERE login = '$_SESSION[login]'"; //requete pour afficher les infos dans le form                        
                         $query = mysqli_query($connexionbd , $requeteinfo);
                         $info_users = mysqli_fetch_all($query, MYSQLI_ASSOC);
 
-                        var_dump($info_users[0]['login']);
-                        if(isset($_POST["valimodif"]) && !empty($_POST))
+                        if(isset($_POST["valimodif"]) && !empty($_POST["login"]) && !empty($_POST["prenom"]) && !empty($_POST["nom"]) && !empty($_POST["oldpassword"]))
                             {  
-                                
-                                $login = $_POST["login"];
-                                $prenom = $_POST["prenom"];
-                                $nom = $_POST["nom"];
-                                $id = $_SESSION["id"];
-                                $requetelogin ="SELECT COUNT(*) FROM utilisateurs WHERE login='$login"; //requete pour vérifier si le login existe
-                                $querylogin = mysqli_query($connexionbd , $requetelogin);
-                                $info_login = mysqli_fetch_all($querylogin, MYSQLI_ASSOC);
-
-                                $requeteupadate = "UPDATE utilisateurs SET prenom='$prenom' , nom='$nom' WHERE id = '$id'";
-                                $queryupdate = mysqli_query($connexionbd , $requeteupadate);                                            
-
-                                if(isset($login))
-                                    if($info_login == 0)                            
+                                if(password_verify($_POST["oldpassword"] , $_SESSION["password"]))
+                                {
+                                    $login = $_POST["login"];
+                                    $prenom = $_POST["prenom"];
+                                    $nom = $_POST["nom"];
+                                    $id = $_SESSION["id"];
+                                    $requetelogin ="SELECT COUNT(*) AS nbr FROM utilisateurs WHERE login='$login'"; //requete pour vérifier si le login existe
+                                    $querylogin = mysqli_query($connexionbd , $requetelogin);
+                                    $info_login = mysqli_fetch_all($querylogin, MYSQLI_ASSOC);                                          
+    
+                                    if($info_login[0]["nbr"] == 0 || $login == $_SESSION["login"])                            
                                         {
-                                            $requeteLupadate = "UPDATE utilisateurs SET login='$login' WHERE id = '$id'";
+                                            $requeteLupadate = "UPDATE utilisateurs SET login='$login' , prenom='$prenom' , nom='$nom' WHERE id = '$id'";
                                             $queryLupdate = mysqli_query($connexionbd , $requeteLupadate); 
+                                            $_SESSION["login"] = $_POST["login"];
+                                            $_SESSION['msg_erreur'] = "Modification prise en compte.";
                                         }
-                                else
+                                    else
+                                        {
+                                            $_SESSION['msg_erreur'] =  "Ce login existe déjà";
+                                        }        
+                                    
+                                    if(isset($_POST["nwpassword"]) && !empty($_POST["nwpassword"]))
+                                        {                                                                       
+                                            if($_POST["nwpassword"] == $_POST["passwordconf"])
+                                                {   
+                                                    $mdpup_hache = password_hash($_POST["nwpassword"], PASSWORD_DEFAULT);             
+                                                    $requetemdpupdate = "UPDATE utilisateurs SET password = '$mdpup_hache' WHERE id = '$id'";
+                                                    $querymdpupdate = mysqli_query($connexionbd , $requetemdpupdate);
+                                                }
+                                            else
+                                                {
+                                                    $_SESSION["msg_erreur"] = "Mot de passe différents";
+                                                }                   
+                                        }     
+                                    
+                                    header("Location:profil.php");                           
+                                }
+                                else    
                                     {
-                                        echo "Ce login existe déjà";
-                                    }
-                                // if(isset($_POST["nwpassword"]) && !empty($_POST["nwpassword"]))
-                                //     {
-                                //         if(password_verify($_POST["oldpassword"] , $_SESSION["password"]))
-                                //             {
-                                //                 $mdpup_hache = password_hash($_POST["nwpassword"], PASSWORD_DEFAULT);
-                                //                 if(password_verify($mdpup_hache , $_SESSION["password"]))
-                                //                     {
-                                //                         if($_POST["nwpassword"] == $_POST["passwordconf"])
-                                //                              {                                                
-                                //                                 $requetemdpuptade = "UPDATE utilisateurs SET password = $mdpup_hache , WHERE id = '$_SESSION[id]'";
-                                //                                 $querymdpupdate = mysqli_query($connexionbd , $requetemdpupadate);
-                                //                             }
-                                //                     }
-                                //             }                                       
-                                //     }     
-                                header("Location:profil.php");                           
+                                        $_SESSION["msg_erreur"] = "Mauvais mot de passe";
+                                    }     
                             }
+                               
                         if($_SESSION["login"] == "admin")
                             {
             ?>
@@ -101,18 +105,24 @@
             <h1>Modification du Profil</h1>
             <form action="profil.php" method="POST" class="formIC">
                 <label for="login">Login :</label>
-                <input type="text" id="insclog" name="login" value="<?php echo $info_users[0]['login'];?>"/> 
+                <input type="text" id="insclog" name="login" value="<?php echo $info_users[0]['login'];?>" required/> 
                 <label for="prenom">Prénom :</label>
-                <input type="text" id="inscpre" name="prenom" value="<?php echo $info_users[0]['prenom'];?>"/>
+                <input type="text" id="inscpre" name="prenom" value="<?php echo $info_users[0]['prenom'];?>" required/>
                 <label for="nom">Nom :</label>
-                <input type="text" id="inscnom" name="nom" value="<?php echo $info_users[0]['nom'];?>"/>
+                <input type="text" id="inscnom" name="nom" value="<?php echo $info_users[0]['nom'];?>" required/>
                 <label for="oldpassword">Ancien Mot de passe :</label>
-                <input type="password" id="oldmdp" name="oldpassword"/>
+                <input type="password" id="oldmdp" name="oldpassword" required/>
                 <label for="nwpassword"> Nouveau Mot de passe :</label>
                 <input type="password" id="inscmdp" name="nwpassword"/>
                 <label for="passwordconf">Confirmation Mot de passe :</label>
                 <input type="password" id="modifconfirm" name="passwordconf"/>
                 <input type="submit" name="valimodif" value="Modifier"/>
+            <?php 
+               if(isset($_SESSION["msg_erreur"]))
+                            {
+                                echo $_SESSION['msg_erreur'];
+                            }                            
+            ?>
             </form>
         </main>
 
